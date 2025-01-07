ea3fe19f4447aa72ad90a07263f9b853b9e85676
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-07 21:40:39 +0300

    Updated heading function

--- classes/layout.php
+++ classes/layout.php
@@ -2,14 +2,18 @@
 class layout{
     public function heading(){
         ?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Urban Links Transport</title>
+<link rel = "icon" type = "image/x-icon" href = "images/favicon.ico" >
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="css/design.css">
+</head>
+<body>
         <?php
     }
         public function footer(){
