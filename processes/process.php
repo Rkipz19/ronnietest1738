427539bccbe082890804642dabd45b137b6ad45e
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-07 22:58:18 +0300

    Added login process

--- processes/process.php
+++ processes/process.php
@@ -13,4 +13,22 @@ class process{
             $stmt->execute();
         }
     }
+    public function login_process(){
+        if(isset($_POST['submit'])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+       
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute();
+            $user = $stmt->fetch();
+            if($user){
+                if(password_verify($password, $user['password'])){
+                    echo '<script>alert("Login successful")</script>';
+                }else{
+                    echo '<script>alert("Invalid password")</script>';
+                }
+            }
+        }
+    }
 }
