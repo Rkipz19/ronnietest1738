61e39e0b899e6379776ae989454f0f0db7896c09
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-08 00:12:39 +0300

    Comparison between password and confirmpassword

---
+++ js/script.js
@@ -0,0 +1,85 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    const passwordInput = document.getElementById("password");
+    const icon = document.getElementById("icon");
+    const confirmPasswordInput = document.getElementById("confirmpassword");
+    const icon1 = document.getElementById("icon1");
+   
+    if(passwordInput.type === "password") {
+      passwordInput.type = "text";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    } else {
+      passwordInput.type = "password";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    }
+    if(confirmPasswordInput.type === "confirmpassword") {
+       confirmPasswordInput.type = "text";
+       icon1.classList.remove("fa-eye-slash");
+       icon1.classList.add("fa-eye");
+     } else {
+       confirmPasswordInput.type = "confirmpassword";
+       icon1.classList.remove("fa-eye");
+       icon1.classList.add("fa-eye-slash");
+     }
+   });
+   
+   function validatePassword(password) {
+       const feedback = document.getElementById("feedback");
+       const strongPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+   
+   
+       let message = "";
+       let IsValid = true;
+   
+       if(password.length < 8) {
+           message = "Password must be at least 8 characters long";
+           IsValid = false;
+       }
+       if(!/^(?=.*[a-z])/.test(password)) {
+           message = "Password must contain at least one lowercase letter";
+           IsValid = false;
+       }
+       if(!/^(?=.*[A-Z])/.test(password)) {
+           message = "Password must contain at least one uppercase letter";
+           IsValid = false;
+       }
+       if(!/^(?=.*[0-9])/.test(password)) {
+           message = "Password must contain at least one number";
+           IsValid = false;
+       }
+       if(!/^(?=.*[!@#\$%\^&\*])/.test(password)) {
+           message = "Password must contain at least one special character";
+           IsValid = false;
+       }
+       if(IsValid){
+           feedback.textContent = "Password is strong";
+           feedback.classList.remove('text-danger');
+           feedback.classList.add('text-success');
+           setTimeout(function(){
+               feedback.textContent = "";
+           }, 2000);
+       } else {
+           feedback.textContent = message;
+           feedback.classList.remove('text-success');
+           feedback.classList.add('text-danger');
+       } 
+   }
+
+   function validateConfirmPassword(confirmpassword) {
+    const feedback1 = document.getElementById("feedback1");
+    var Password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmpassword").value;
+    if(confirmPassword !== Password) {
+        feedback1.textContent = "Passwords do not match";
+        feedback1.classList.remove('text-success');
+        feedback1.classList.add('text-danger');
+    } else {
+        feedback1.textContent = "Passwords match";
+        feedback1.classList.remove('text-danger');
+        feedback1.classList.add('text-success');
+        setTimeout(function(){
+            feedback1.textContent = "";
+        }, 3000);
+    }
+}
