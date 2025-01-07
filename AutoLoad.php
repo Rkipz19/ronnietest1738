7301f9c03d20763b59a1faa3c2ee5e19fba935fd
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-08 01:13:30 +0300

    New AutoLoad

---
+++ AutoLoad.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Method Auto Load
+
+function classAutoLoad($classname){
+    $directories = ["classes", "contents", "forms", "processes", "global", "menus"];
+
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once($filename);
+        }
+    }
+}
+spl_autoload_register('classAutoLoad');
+
