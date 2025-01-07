8eaa062fae5ca5a6fff0dd6df45022be3b1763f5
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-07 21:31:57 +0300

    Added carousel function

--- contents/pagecontent.php
+++ contents/pagecontent.php
@@ -33,4 +33,48 @@ class pagecontent{
       </section>
       <?php
     }
+    public function carousel(){
+        ?>
+        <section>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="images/agriculture-trucks.jpg" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>WELCOME TO URBAN LINKS TRANSPORT</h5>
+                <p>Where we provide transportation for various agricultural goods for farmers</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="images/loaders.jpg" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Staff</h5>
+                <p>Entails drivers and loaders, who ensure that the agricultural goods are transported safely.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="images/pickup.jpg" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Our services</h5>
+                <p>The company transports produce such as cereal, livestock, farm inputs,and perishable goods.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        </section>
+        <?php
+    }
 }
