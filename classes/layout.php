1940120342d6367a726df6d9cbf2efe58037072f
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-07 21:42:30 +0300

    Modified footer function added js script links

--- classes/layout.php
+++ classes/layout.php
@@ -18,8 +18,11 @@ class layout{
     }
         public function footer(){
             ?>
-        </body>
-        </html>
-        <?php
+            <!--Bootstrap bundle with popper-->
+            <script src = "js/script.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            </body>
+            </html>
+            <?php
     }
 }
