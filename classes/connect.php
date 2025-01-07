3b9de6b73f4a0527e7574b9f055eac80f53a6ccd
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-07 22:14:59 +0300

    Modified connect.php

--- classes/connect.php
+++ classes/connect.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Sigei1738";
 
+class connection{
+private $servername = "localhost";
+private $username = "root";
+private $password = "Sigei1738";
+private $dbname = "urbanlinktransport";
+
+protected function connect(){
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=urbanlinktransport", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+    $rn = 'mysql:host'.$this->servername.';dbname='.$this->dbname;
+    $conn = new PDO($rn, $this->username, $this->password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+    //echo "Connection successful";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
+}
+}
